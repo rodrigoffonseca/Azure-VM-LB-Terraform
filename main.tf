83b89b591bb6684bb0e89098a926d6c4296dd38d
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2020-03-18 16:13:08 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # Configure the Microsoft Azure Provider.
 provider "azurerm" {
     version = "=2.0.0"
-    subscription_id = "cce01445-8719-4563-b5b7-37b26250b020"
-    client_id       = "e779ba7b-619d-4617-a964-66d743f02887"
-    client_secret   = "2f053691-cce4-435c-84ee-917e30b84aa8"
-    tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+    subscription_id = "Enter your azure subscription ID"
+    client_id       = "Enter your azure Service Principal App ID"
+    client_secret   = "Enter your azure Service Principal Client Secrete"
+    tenant_id       = "Enter your azure Tenant ID"
     #service principal information to login 
     features {}
 }
@@ -278,14 +278,14 @@ data "azurerm_public_ip" "test" {
 }
 
 #Terraform backend config for Remote State in Azure Storage Account
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-foca-storage"
-    storage_account_name = "safocainfra"
-    container_name       = "terraformstate"
-    key                  = "terraform.tfstate"
-  }
-}
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "rg-foca-storage"
+#    storage_account_name = "safocainfra"
+#    container_name       = "terraformstate"
+#    key                  = "terraform.tfstate"
+#  }
+#}
 
 # Locals
 
